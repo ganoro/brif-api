12844235174e76a83136c2c35cb553bfb3198e32
Roy Ganor <ganoro@gmail.com>
2013-08-21 17:32:44 +0300

    fail on missing parameters

--- server.js
+++ server.js
@@ -2,13 +2,28 @@ var express = require('express');
 var config = require('./config.js');
 var Kaiseki = require('kaiseki');
 
+/**
+ * initialize express app
+ */ 
 var app = express();
+
+/**
+ * initialize parse app
+ */ 
 var parse = new Kaiseki(config.kaiseki_app_id, config.kaiseki_rest_api_key);
 
 app.use(express.bodyParser());
 
 app.post('/signin', function(req, res){
   	var email = req.body.email;
+  	var name = req.body.name;
+  	var code = req.body.code;
+
+  	if (email == null || name == null || code == null) {
+  		res.send(400, "Bad Request, missing parameter (email, name or code)");
+  		return; 
+  	}
+
   	res.send(email);
 });
 
