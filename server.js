1a47954b3e45de2c7db7afb884a6702bdcb20575
Roy Ganor <ganoro@gmail.com>
2013-09-01 14:04:01 +0300

    fix allow origin

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ var Users = parse.Object.extend("Users");
  */ 
 app.use(express.bodyParser());
 
+app.get('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 /**
  * routes
  */ 
