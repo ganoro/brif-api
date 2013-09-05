d3b8297ccf41e504f5632938652cf660c596aad1
Roy Ganor <ganoro@gmail.com>
2013-09-05 18:45:43 +0300

    refactoring

---
+++ route.notifications.js
@@ -0,0 +1,31 @@
+/**
+ * Groups events
+ */
+exports.groups = function(req, res){
+
+};
+
+/**
+ * Messages events
+ */
+exports.messages = function(req, res){
+
+};
+
+
+var customersNotifications = {};
+
+	//subscribe to a topic
+    // var handle = minpubsub.subscribe("/some/topic", function(msg){
+    //     console.log(msg);
+    // });
+
+    // //publish topic a few times
+    // minpubsub.publish("/some/topic", ["first time"]);
+    // minpubsub.publish("/some/topic", ["second time"]);
+
+    // //unsubscribe from the topic
+    // minpubsub.unsubscribe(handle);
+
+
+
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+/**
+ * Load libraries 
+ */ 
 var express = require('express');
 var config = require('./config.js');
 var request = require('request');
 var xml2js = require('xml2js').parseString;
 var $ = require('jquery').create();
 var parse = require('parse').Parse;
+var minpubsub = require('minpubsub/minpubsub');
+
+/**
+ * Load routes
+ */ 
+var routes = {}; 
+routes['auth'] = require('./route.auth.js');
+routes['notifications'] = require('./route.notifications.js');
 
 /**
  * initialize express app
@@ -24,187 +35,17 @@ app.use(express.bodyParser());
 app.all('*', function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-next();
+	next();
  });
 
 /**
- * routes
+ * Setup routes
  */ 
-app.get('/auth/signin', function(req, res){
-
-	// error handling
-	var error = req.query.error;
-	if (error != null) {
-		sendPostMessage(res, 'cancel')
-		return;
-	} 
-
-  	// validation check
-  	var code = req.query.code;
-  	var origin = req.query.state;
-  	if (code == null || origin == null) {
-		// TODO internal error 404?
-		sendPostMessage(res, 'internal_error')
-  		return; 
-  	}
-
-  	// exchange code for (a refreshable) token
-  	var google_config = eval("config.google_config_" + origin);
-  	var form = {
-		code: code, 
-		client_id : google_config.client_id,
-		client_secret : google_config.client_secret,
-		redirect_uri : google_config.redirect_uris[0],
-		grant_type : 'authorization_code'
-    };
-  	request({
-	    method: 'POST', 
-	    uri: 'https://accounts.google.com/o/oauth2/token',
-	    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-	    form : form
-	}, function(e, r, body) {
-		var data = JSON.parse(body);
-		if (data.access_token != null && data.expires_in != null && data.refresh_token != null) {
-			sendPostMessage(res, 'accept');
-			console.log(body);
-			data.origin = origin;
-			processSignup(data);
-		} else {
-			sendPostMessage(res, 'google_error' +  data.error);
-		}
-	});
-});
-
-app.post('/auth/mobile-signin', function(req, res){
-  	// validation check
-  	var access_token = req.body.access_token;
-  	if (access_token == null) {
-		// TODO internal error 404?
-		res.send(400, 'missing parameters (access_token, ...)');
-  		return; 
-  	}
-
-	var data = { 
-		access_token : req.body.access_token,
-		refresh_token : req.body.refresh_token,
-		token_type : req.body.token_type,
-		expires_in : parseInt(req.body.expires_in),
-		id_token : req.body.id_token,
-		origin : 'ios'
-	};
-
-	processSignup(data);
-	res.send(JSON.stringify( { message : "user authenticated successfully", data : data } ));
-});
-
-app.post('/auth/refresh-token', function(req, res) {
-
-	// validate params
-  	var email = req.body.email;
-  	if (email == null) {
-		res.send(400, 'missing parameter (email)');
-  	}
-
-  	var query = new parse.Query(Users);
-  	query.equalTo("email", email);
-	query.first({
-		success: function(user) {
-			if (user) {
-				return refreshToken(user, res);
-			} else {
-				res.send(400, 'email not found ' + email);
-			}	
-		},
-		error: function(error) {
-			res.send(400, error.message);
-			console.log("Error: " + error.code + " " + error.message);
-		}
-	});
-});
-
-/**
- * post message to opener
- */
- var refreshToken = function(user, res) {
-	var origin = user.get("origin");
-	var refresh_token = user.get("refresh_token");
-	var google_config = eval("config.google_config_" + origin);
-
-	var form = {
-		refresh_token : refresh_token,
-		client_id : google_config.client_id,
-		client_secret : google_config.client_secret,
-		grant_type : 'refresh_token',
-	};
-	request({
-	    method: 'POST', 
-	    uri: 'https://accounts.google.com/o/oauth2/token',
-	    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-	    form : form
-	}).pipe(res);
-}
-
-/**
- * post message to opener
- */
-var sendPostMessage = function(res, message) {
-	res.send("<script>window.opener.postMessage('" + message + "', '*');window.close();</script>"); 
-}
-
-/**
- * completes signup
- */
-var processSignup = function(data) {
-	request.get('https://www.googleapis.com/oauth2/v2/userinfo?access_token=' + data.access_token, function(e, r, body) {
-		console.log(body);
-		var user = JSON.parse(body);
-
-		request.get('https://www.google.com/m8/feeds/contacts/default/full/?max-results=1&access_token=' + data.access_token, function(e, r, body) {
-			xml2js(body, function(error, result) {
-				console.log(result);
-				var email = result.feed.id[0];
-				var token_refresh_time = new Date();
-				token_refresh_time.setSeconds(token_refresh_time.getSeconds() + 3600);
-				var user_data = $.extend({}, { 
-					email : email }, 
-					user, 
-					data, 
-					{ 'token_refresh_time' : token_refresh_time 
-				});
-				storeUserData(user_data);
-			});
-		});
-	}); 
-}
-
-/**
- * store user data in parse
- */
-var storeUserData = function(user_data) {
-	delete user_data.id;
-	console.log(user_data);
-
-	var query = new parse.Query(Users);
-	query.equalTo("email", user_data.email);
-	console.log("new user: " + user_data.email);
-	query.first({
-		success: function(object) {
-			var u = (object ? object : new Users());
-			u.set(user_data);
-			u.save(null, {
-				success : function(o) {
-					console.log("user attributes saved!");
-				},
-				error : function(o, e) {
-					console.log("Error: " + error.code + " " + error.message);
-				} 
-			});
-		},
-		error: function(error) {
-			console.log("Error: " + error.code + " " + error.message);
-		}
-	});
-}
+app.get ('/auth/signin'          , routes['auth'].signin);
+app.post('/auth/mobile-signin'   , routes['auth'].mobile_signin);
+app.post('/auth/refresh-token'   , routes['auth'].refresh_token);
+app.post('/notification/group'   , routes['notifications'].groups);
+app.post('/notification/messages', routes['notifications'].messages);
 
 /**
  * error handling
