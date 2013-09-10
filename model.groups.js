743d54957460b968d320c17dec11a8e20e55cfac
Roy Ganor <ganoro@gmail.com>
2013-09-11 01:01:30 +0300

    fix syntax error

--- model.groups.js
+++ model.groups.js
@@ -10,7 +10,7 @@ var Users = model.parse.Object.extend("Groups");
  * success() and error() functions required in opts
  */ 
 exports.findByUser = function(user_id, opts) {
-	console.log("per_page " + opts.per_page ;
+	console.log("per_page " + opts.per_page) ;
   	console.log("limit " + (opts.page - 1)*opts.per_page);
 
   	var query = new model.parse.Query(Users);
