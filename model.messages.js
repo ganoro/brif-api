4fba80ca70ef2e2532abf1b20234a37c1db0aeac
Roy Ganor <ganoro@gmail.com>
2013-09-14 23:02:57 +0300

    update and query

--- model.messages.js
+++ model.messages.js
@@ -8,10 +8,15 @@ var Messages = model.parse.Object.extend("Messages");
  * paremetrs required - group_id, user_id
  */ 
 exports.findByGroupId = function(opts) {
-  	var query = new model.parse.Query(Messages);
-  	query.equalTo("thread", opts.group_id);
-  	query.equalTo("user", opts.user_id);
+  	var query1 = new model.parse.Query(Messages);
+  	query1.equalTo("thread", opts.group_id);
+
+  	var query2 = new model.parse.Query(Messages);
+  	query2.equalTo("user", opts.user_id);
+
+	var query = Parse.Query.and(query1, query2);
   	query.limit(opts.per_page).skip(opts.page*opts.per_page);
+
 	query.find(opts);
 }
  
