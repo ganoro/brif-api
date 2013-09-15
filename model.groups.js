a58a3b3a3335be9692c4e2f95e352722d45ff74f
Roy Ganor <ganoro@gmail.com>
2013-09-15 08:43:57 +0300

    changed user to user_id

--- model.groups.js
+++ model.groups.js
@@ -11,7 +11,7 @@ var Groups = model.parse.Object.extend("Groups");
  */ 
 exports.findByUser = function(user_id, opts) {
   	var query = new model.parse.Query(Groups);
-  	query.equalTo("user", user_id);
+  	query.equalTo("user_id", user_id);
   	query.limit(opts.per_page).skip(opts.page*opts.per_page);
 	query.find(opts);
 }
