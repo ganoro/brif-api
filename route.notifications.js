6500fc71a618b5f1b234e5410c2de89bea98292d
Roy Ganor <ganoro@gmail.com>
2013-10-21 12:05:53 +0300

    remove console logs

--- route.notifications.js
+++ route.notifications.js
@@ -170,7 +170,6 @@ var onSocketChannelsSend = function (socket, data, user) {
 
 var onSocketGroupsFetch = function (socket, data, user) {
 	console.log("onSocketGroupsFetch");
-	console.log(data);
 	if (data.groups == null) {
 		// TODO internal error
 	}
@@ -318,7 +317,6 @@ var messagesFetchAll = function(socket, data, user) {
 
 var groupsFetch = function(socket, data, user) {
 	console.log("groupsFetch()");
-	console.log(data);
 	var url = 'https://www.google.com/m8/feeds/groups/default/full/batch';
 	var headers = { "Authorization" : "Bearer " + user.access_token };
 	var body = templates.compile('retrieve_groups', { groups : data.groups });
--- server.js
+++ server.js
@@ -58,7 +58,6 @@ io.sockets.on('connection', function (socket) {
 	// proxy, enables socket and user details as parameters
 	var proxy = function(callback) {
 		return function(data) {
-			console.log(data);
 			socket.get('user', function(err, result) {
 				var user = JSON.parse(result);
 				callback(socket, data, user);
