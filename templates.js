05c460364bbe5d113a9fb5959c5a50fd1992d3de
Roy Ganor <ganoro@gmail.com>
2013-10-22 20:22:41 +0300

    add new group template

--- templates.js
+++ templates.js
@@ -6,16 +6,21 @@ var _ = require('underscore');
 
 var templates = {};
 
-// retrieve groups xml
-fs.readFile('./templates/retrieve_groups.xml', function (err, data) {
-  if (err) throw err;
-  templates['retrieve_groups'] = data.toString('utf8');;
-});
+var load = function(name, filename) {
+	// retrieve groups xml
+	fs.readFile('./templates/' + filename, function (err, data) {
+	  if (err) throw err;
+	  templates[name] = data.toString('utf8');;
+	});
+}
 
 var compile = function(name, parameters) {
 	var text = templates[name];
 	return _.template(text, parameters);
 }
+
+load('retrieve_groups', 'retrieve_groups.xml');
+load('new group', 'new_group.xml');
 
 module.exports = {
 	compile : compile
