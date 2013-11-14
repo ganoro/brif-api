d40304b1fa334bf75c04e561ee0a00780f68c1d0
Roy Ganor <ganoro@gmail.com>
2013-11-14 14:12:49 +0200

    header line

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.use(express.bodyParser());
 app.all('*', function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Credentials', true);
 	next();
  });
 
