499b7292152d6ac0f1af33153f167988638ee43e
Roy Ganor <ganoro@gmail.com>
2013-12-18 13:00:09 +0200

    remove console.logs

--- model.messages.js
+++ model.messages.js
@@ -27,20 +27,17 @@ var findByRecipientsId = function(opts) {
  * paremetrs required - google_msg_id (array)
  */ 
 var findByGoogleMsgId = function(opts) {
+  console.log("findByGoogleMsgId()");
+
   if (opts.google_msg_id.length == 0) {
     return opts.success([]);
   }
-
-	console.log("findByGoogleMsgId()");
-  console.log(opts.google_msg_id);
-  console.log(opts.user_id);
 
   var queries = [];
   for (var i = 0; i < opts.google_msg_id.length; i++) {
     var Messages = model.parse.Object.extend("Messages_" + opts.user_id);
     var query = new model.parse.Query(Messages);
   	query.equalTo("google_msg_id", opts.google_msg_id[i]);
-  	console.log(opts.google_msg_id[i]);
   	queries.push(query);
   };
   var agg = model.parse.Query.or.apply(null, queries);
