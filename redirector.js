364a276a8a7ef6c1fc9d75349f8b0be0ef715d03
Roy Ganor <ganoro@gmail.com>
2014-02-16 08:45:03 +0200

    minor fix for redirect

--- redirector.js
+++ redirector.js
@@ -6,7 +6,7 @@ var express = require("express"),
     port    = 80;
 
 app.all('*', function(req, res, next) {
-    res.redirect('http://www.brif.us/' + req.path);
+    res.redirect('http://www.brif.us' + req.path);
  });
 
 app.listen(port);
