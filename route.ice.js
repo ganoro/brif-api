c00791de3f5fa4a48e333c623570be679337242e
Roy Ganor <ganoro@gmail.com>
2014-05-19 10:14:23 +0300

    create room before joining it

--- route.ice.js
+++ route.ice.js
@@ -5,6 +5,7 @@ var request = require('request');
 
 var onSocketCreateRoom = function (socket, data, user) {
 	console.log("onSocketCreateRoom");
+
   	var form =  {
 		ident: "roybrif",
 		secret: "e8231a53-e1b7-406e-81fa-367d46da6778",
@@ -15,14 +16,26 @@ var onSocketCreateRoom = function (socket, data, user) {
 	};
 	request({
 		method: 'POST', 
-		uri: 'https://api.xirsys.com/getIceServers',
+		uri: 'https://api.xirsys.com/addRoom',
 		headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
 		form : form
 	}, function(e, r, body) {
+		if (e) {
+			return socket.emit('ice:room', { error : e })
+		}
 		var data = JSON.parse(body);
-		console.log("data", data.d);
-		console.log("form", form)
-		socket.emit('ice:room', { data : data.d })
+		if (data.s == 201 || data.s == 409) {
+			request({
+				method: 'POST', 
+				uri: 'https://api.xirsys.com/getIceServers',
+				headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+				form : form
+			}, function(e, r, body) {
+				var data = JSON.parse(body);
+				console.log("data", data);
+				socket.emit('ice:room', { data : data.d })
+			});
+		}
 	});
 }
 
