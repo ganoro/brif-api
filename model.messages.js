9e7312bfdbccfa2abf9a3610d988ccaebe3bf518
Roy Ganor <ganoro@gmail.com>
2014-07-15 14:40:00 +0300

    add google trd id to get message by google id

--- model.messages.js
+++ model.messages.js
@@ -49,14 +49,14 @@ var findByGoogleMsgId = function(opts) {
       query.exists("unsubscribe");
     }
     if (opts.is_select_special) {
-      query.select("unsubscribe", "google_msg_id", "recipients", "recipients_names", "recipients_id", "message_id", "sent_date");
+      query.select("unsubscribe", "google_msg_id", "google_trd_id", "recipients", "recipients_names", "recipients_id", "message_id", "sent_date");
     }
   	query.equalTo("google_msg_id", opts.google_msg_id[i].toString());
   	queries.push(query);
   };
   var agg = model.parse.Query.or.apply(null, queries);
   if (opts.is_select_special) {
-    agg.select("unsubscribe", "google_msg_id", "recipients", "recipients_names", "recipients_id", "message_id", "sent_date");
+    agg.select("unsubscribe", "google_msg_id", "google_trd_id", "recipients", "recipients_names", "recipients_id", "message_id", "sent_date");
   }
   agg.find(opts);       
 }
