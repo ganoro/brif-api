91977b50fdb1549c3543e0c1687ac9a27b3c8684
Roy Ganor <ganoro@gmail.com>
2014-08-16 19:22:35 -0400

    check for auth

--- route.notifications.js
+++ route.notifications.js
@@ -81,7 +81,9 @@ var onSocketSetup = function(socket, data, user) {
 				token : function(err, token, access_token) {
 					if (err) {
 						// TODO : internal error
-						
+						if (!data.auth) {
+							return socket.emit('setup:error');
+						}
 						var auth = data.auth;
 						user.set("refresh_token", auth.code);
 						user.set("origin", config.getStateByClientId(auth.client_id));
