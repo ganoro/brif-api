02698401beb78f7d5104dfaba702eb58b81f79a4
Roy Ganor <ganoro@gmail.com>
2014-08-16 19:51:08 -0400

    revert to last solution

--- route.notifications.js
+++ route.notifications.js
@@ -81,22 +81,7 @@ var onSocketSetup = function(socket, data, user) {
 				data: data,
 				token : function(err, token, access_token) {
 					if (err) {
-						console.log(auth);
-						// TODO : internal error
-						console.log(data)
-						if (!data.auth) {
-							return socket.emit('setup:error');
-						}
-						user.set("refresh_token", data.auth.code);
-						user.set("origin", config.getStateByClientId(data.auth.client_id));
-						user.save(null, {
-							success: function(user) {
-								onSocketSetup(socket, { email : data.email } , user);
-							},
-							error: function() {
-								socket.emit('setup:error');
-							}
-						});
+						socket.emit('setup:error');
 						return console.log(err);
 					} else {
 						var user_data = { 
